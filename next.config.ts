08c28ecea0cf14091b39fa4871e34f939b49bbef
yukishafa <maheswari.yukishafa@gmail.com>
2026-03-11 09:34:26 +0700

    chore: update next/image configuration to use remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["image.tmdb.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/**",
+      },
+    ],
   },
 };
 
