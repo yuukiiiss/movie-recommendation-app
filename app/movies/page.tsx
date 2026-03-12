c15c42c91ebe9603cd930a7e2d65d9f7f768d0a6
yukishafa <maheswari.yukishafa@gmail.com>
2026-03-12 21:47:01 +0700

    feat: add /movies page route

---
+++ app/movies/page.tsx
@@ -0,0 +1,18 @@
+import { getTrendingMovies } from "@/lib/tmdb"
+import MovieCard from "@/components/MovieCard"
+
+export default async function MoviesPage() {
+  const movies = await getTrendingMovies()
+
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold mb-6">All Movies</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {movies.map((movie: any) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </main>
+  )
+}
